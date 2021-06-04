a239a19ba886847a5fbc9395bf4e5e18e1d4fc79
Jason Anderson <jas.nanders.n@gmail.com>
2021-06-04 12:03:21 -0700

    formatting

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  });
 
 // routes
 app.use(require("./routes/api.js"));
